384750a8b06777165f23a0ac591beb7ca6fd3a5a
Tanh Nguyen <nvtanh.dev@gmail.com>
2023-02-12 12:56:55 +0700

    update actions

--- auto_versioning.sh
+++ auto_versioning.sh
@@ -5,6 +5,6 @@ set -e
 perl -i -pe 's/^(version:\s+\d+\.\d+\.\d+\+)(\d+)$/$1.($2+1)/e' ./pubspec.yaml
 
 # # Commit and tag this change.
-# version=`grep 'version: ' ./pubspec.yaml | sed 's/version: //'`
-# git commit -m "Bump version to $version" ../pubspec.yaml
-# git tag $version
+version=`grep 'version: ' ./pubspec.yaml | sed 's/version: //'`
+git commit -m "Bump version to $version" ./pubspec.yaml
+git tag $version
